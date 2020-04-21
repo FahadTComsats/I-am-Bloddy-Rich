4441ca3ec309e0af889da68cecccd957f85639ca
Fahad Tariq <47610084+FahadTComsats@users.noreply.github.com>
2020-04-21 20:09:16 +0500

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Fahad Tariq on 21/04/2020.
+//  Copyright © 2020 Fahad Tariq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
